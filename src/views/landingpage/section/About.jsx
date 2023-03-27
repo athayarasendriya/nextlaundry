8de036b7243cb4f136d53713050f5a20c0ac04e2
athayarasendriya <101735103+athayarasendriya@users.noreply.github.com>
2023-03-27 15:13:06 +0700

    Add files via upload

--- src/views/landingpage/section/About.jsx
+++ src/views/landingpage/section/About.jsx
@@ -1,26 +1,33 @@
-import { about } from '../../../assets';
+import { about, ellipseclothes, ellipseyear } from '../../../assets';
 import styles from '../../../style';
 
 const About = () => {
     return (
         <section id="About" className={`flex flex-row flex-col md:ml-10`} >
-         <div className="flex flex-row">
-          <img src={ about }  className="w-[400px] h-[400px]"/>
-            <div className="flex flex-col">
-            <h1 className={`text-right w-full ${styles.heading2}`}>
-                Better Way, <br/>For Better Day!
-            </h1>
-            <p className={`pt-5 text-right ${styles.paragraph} container w-3/4`}>
-              Lorem ipsum dolor sit amet consectetur. Purus tempus tincidunt massa laoreet in non.
-            </p>
-              <div className="flex-row">
-                  icon 15+
-                  icon 60+
-              </div>
-          </div>
-          <div className={`absolute bg-purple container w-[16px] h-[150px] rounded-[50px] mt-10 item-right`}></div>
-  </div>
-        </section>
+         <div className="flex flex-row flex-grow grow pt-14 pr-14 mr-14 ml-14">
+            <div className={`flex flex-col font-righteous font-semibold z-20`}>
+                <p className={`flex flex-row underline underline-offset-4 text-2xl ${styles.copyright}`}>
+                    About
+                </p>
+                <img src={ about }  className="w-[500px] h-[500px]"/>
+            </div>
+            <div className="flex flex-col flex-grow grow align-end items-end pt-10 pr-10">
+                <h1 className={`text-right w-full ${styles.heading2}`}>
+                    Better Way, <br/>For Better Day!
+                </h1>
+                <p className={`pt-5 text-right ${styles.paragraph} container w-2/4`}>
+                    Lorem ipsum dolor sit amet consectetur. Purus tempus tincidunt massa laoreet in non.
+                </p>
+                <div className="flex gap-20 ">
+                    <img src={ ellipseyear } alt="year" className="w-[180px] h-[180px]"/>
+                    
+                    <img src={ ellipseclothes } alt="year" className="w-[180px] h-[180px]"/>
+                </div>
+            </div>
+            <div className={`align-end items-end items-right bg-magenta container w-[16px] h-[150px] rounded-[50px] mt-14 `}></div>
+        </div>
+        
+    </section>
     )
 }
 
